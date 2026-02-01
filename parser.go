60c26f6e897b90430fc167372cdf4714caca98ab
N.Steklov <avitnik@gmail.com>
2026-02-01 14:57:36 +0300

    feat(cli)/sub-commands rags parsing

--- parser.go
+++ parser.go
@@ -13,22 +13,6 @@ var (
 	ErrMissingID      = errors.New("missing ID")
 )
 
-// func declarFlags(args []string) (Flags, error) {
-// 	fs := flag.NewFlagSet("todo", flag.ContinueOnError)
-
-// 	var f Flags
-// 	fs.StringVar(&f.Command, "cmd", "list", "command name string")
-// 	fs.StringVar(&f.Title, "title", "", "task title string")
-// 	fs.IntVar(&f.ID, "id", 0, "task id int")
-
-// 	if err := fs.Parse(args); err != nil {
-// 		return Flags{}, fmt.Errorf("Unknown arguments")
-// 	}
-
-// 	return f, nil
-
-// }
-
 func parseAdd(args []string) (Command, error) {
 	if len(args) == 0 {
 		return nil, ErrMissingTitle
@@ -93,54 +77,6 @@ func parseDone(args []string) (Command, error) {
 
 	return &DoneCommand{ID: id}, nil
 }
-
-// func parseArgs(args []string) (Command, error) {
-
-// 	if len(args) > 0 && args[0] == os.Args[0] {
-// 		args = args[1:]
-// 	}
-
-// 	f, err := declarFlags(args)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	switch f.Command {
-// 	case "add":
-// 		if len(f.Title) == 0 {
-// 			return nil, ErrMissingTitle
-// 		}
-
-// 		return &AddCommand{
-// 			Title: f.Title,
-// 		}, nil
-
-// 	case "done":
-// 		if f.ID == 0 {
-// 			return nil, ErrMissingID
-// 		}
-
-// 		return &DoneCommand{
-// 			ID: f.ID,
-// 		}, nil
-
-// 	case "remove":
-// 		if f.ID == 0 {
-// 			return nil, ErrMissingID
-// 		}
-
-// 		return &RemoveCommand{
-// 			ID: f.ID,
-// 		}, nil
-
-// 	case "list":
-// 		return &ListCommand{}, nil
-
-// 	default:
-// 		return nil, ErrUnknownCommand
-// 	}
-
-// }
 
 func parseArgs(args []string) (Command, error) {
 
